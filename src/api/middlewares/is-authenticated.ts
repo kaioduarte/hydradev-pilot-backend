cdb6767754e75e1cedd75536dbb698def8f0a0bd
Kaio Duarte <duartedossantos@gmail.com>
2020-03-03 19:28:17 +0000

    remove callback from jwt.verify

--- src/api/middlewares/is-authenticated.ts
+++ src/api/middlewares/is-authenticated.ts
@@ -16,13 +16,12 @@ export function isAuthenticated(req: Request, _res, next) {
     token = req.query.token;
   }
 
-  jwt.verify(token, config.jwtSecret, async (err, decoded) => {
-    if (err) {
-      throw new ApiError(`JWT Error: ${err.message}`, HttpStatus.BAD_REQUEST);
-    }
-
+  try {
+    const decoded: any = jwt.verify(token, config.jwtSecret);
     req.token = decoded;
+  } catch (err) {
+    throw new ApiError(`JWT Error: ${err.message}`, HttpStatus.BAD_REQUEST);
+  }
 
-    return next();
-  });
+  return next();
 }
