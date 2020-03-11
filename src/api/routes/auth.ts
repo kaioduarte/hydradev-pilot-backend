201a09af222370897d7e1214acbbadc4613d08a2
Kaio Duarte <duartedossantos@gmail.com>
2020-03-11 13:08:23 +0000

    update auth routes with celebrate constraints

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Container } from 'typedi';
 import { Router, Request, Response } from 'express';
 import { celebrate, Joi } from 'celebrate';
+import HttpStatus from 'http-status-codes';
 import AuthService from '@/services/auth.service';
 import { ISignUpDto } from '@/interfaces/dto/sign-up.dto';
-import HttpStatus from 'http-status-codes';
 import { ISignInDto } from '@/interfaces/dto/sign-in.dto';
+import { joiOptions } from '@/config/celebrate';
 
 const route = Router();
 
@@ -16,37 +17,55 @@ export default (app: Router) => {
 
   route.post(
     '/signup',
-    celebrate({
-      body: Joi.object({
-        name: Joi.string().required(),
-        username: Joi.string().required(),
-        password: Joi.string().required(),
-      }),
-    }),
+    celebrate(
+      {
+        body: Joi.object({
+          name: Joi.string()
+            .trim()
+            .min(3)
+            .max(255)
+            .required(),
+          username: Joi.string()
+            .trim()
+            .min(3)
+            .max(64)
+            .required(),
+          password: Joi.string()
+            .trim()
+            .min(6)
+            .max(32)
+            .required(),
+        }),
+      },
+      joiOptions,
+    ),
     async (req: Request, res: Response) => {
       const { user, token } = await authService.signUp(req.body as ISignUpDto);
-
-      return res.status(HttpStatus.OK).jsend.success({
-        user,
-        token,
-      });
+      return res.status(HttpStatus.OK).jsend.success({ user, token });
     },
   );
 
   route.post(
     '/signin',
-    celebrate({
-      body: Joi.object({
-        username: Joi.string().required(),
-        password: Joi.string().required(),
-      }),
-    }),
+    celebrate(
+      {
+        body: Joi.object({
+          username: Joi.string()
+            .trim()
+            .min(3)
+            .required(),
+          password: Joi.string()
+            .trim()
+            .min(6)
+            .max(32)
+            .required(),
+        }),
+      },
+      joiOptions,
+    ),
     async (req: Request, res: Response) => {
       const { user, token } = await authService.signIn(req.body as ISignInDto);
-      return res.status(HttpStatus.OK).jsend.success({
-        user,
-        token,
-      });
+      return res.status(HttpStatus.OK).jsend.success({ user, token });
     },
   );
 
